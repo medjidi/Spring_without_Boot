ef8d28bfd2c89aea70098a6615523665d03bb1c6
medjidi <medjidi@yandex.ru>
2022-11-17 18:40:03 +0300

    Rename

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package web.Controller;
+package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,11 +1,11 @@
-package web.Controller;
+package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import web.Service.UserService;
+import web.service.UserService;
 import web.models.User;
 
 @Controller
